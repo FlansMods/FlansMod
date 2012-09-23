745785db1feedc4e1cb6c619677eadee3e7458fe
Tarun Singh <tarun2011.ts@gmail.com>
2012-09-23 22:13:02 +1000

    Guns crash fix, also fix broken Events.

--- src/common/co/uk/flansmods/common/FlansModPlayerData.java
+++ src/common/co/uk/flansmods/common/FlansModPlayerData.java
@@ -1,7 +1,11 @@
 package co.uk.flansmods.common;
 
+import net.minecraft.src.EntityPlayer;
+
 public class FlansModPlayerData 
 {
 	public EntityMG mountingGun;
 	
+	//TODO Unknown purpose!
+	public FlansModPlayerData(EntityPlayer player) {}
 }
