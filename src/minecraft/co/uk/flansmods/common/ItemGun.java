391c724a8e75fcd8471b2fe10b2f55e2e842a72c
James Flannery <jamioflan@hotmail.co.uk>
2012-12-24 10:41:15 +0000

    Fixed client side ref in ItemGun

--- src/minecraft/co/uk/flansmods/common/ItemGun.java
+++ src/minecraft/co/uk/flansmods/common/ItemGun.java
@@ -10,6 +10,7 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumMovingObjectType;
 import net.minecraft.util.MathHelper;
 import net.minecraft.util.MovingObjectPosition;
+import net.minecraft.util.Vec3;
 import net.minecraft.world.World;
 
 import org.lwjgl.input.Mouse;
@@ -353,7 +354,23 @@ public class ItemGun extends Item
 	{
 		if (type.deployable)
 		{
-			MovingObjectPosition look = FMLClientHandler.instance().getClient().objectMouseOver;
+			float f = 1.0F;
+	        float f1 = entityplayer.prevRotationPitch + (entityplayer.rotationPitch - entityplayer.prevRotationPitch) * f;
+	        float f2 = entityplayer.prevRotationYaw + (entityplayer.rotationYaw - entityplayer.prevRotationYaw) * f;
+	        double d = entityplayer.prevPosX + (entityplayer.posX - entityplayer.prevPosX) * (double)f;
+	        double d1 = (entityplayer.prevPosY + (entityplayer.posY - entityplayer.prevPosY) * (double)f + 1.6200000000000001D) - (double)entityplayer.yOffset;
+	        double d2 = entityplayer.prevPosZ + (entityplayer.posZ - entityplayer.prevPosZ) * (double)f;
+	        Vec3 vec3d = Vec3.createVectorHelper(d, d1, d2);
+	        float f3 = MathHelper.cos(-f2 * 0.01745329F - 3.141593F);
+	        float f4 = MathHelper.sin(-f2 * 0.01745329F - 3.141593F);
+	        float f5 = -MathHelper.cos(-f1 * 0.01745329F);
+	        float f6 = MathHelper.sin(-f1 * 0.01745329F);
+	        float f7 = f4 * f5;
+	        float f8 = f6;
+	        float f9 = f3 * f5;
+	        double d3 = 5D;
+	        Vec3 vec3d1 = vec3d.addVector((double)f7 * d3, (double)f8 * d3, (double)f9 * d3);
+	        MovingObjectPosition look = world.rayTraceBlocks_do(vec3d, vec3d1, true);
 			if (look != null && look.typeOfHit == EnumMovingObjectType.TILE)
 			{
 				if (look.sideHit == 1)
