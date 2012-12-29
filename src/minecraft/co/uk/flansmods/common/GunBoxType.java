da02f4e13325a59385e2f9f0ec70a70a7f92d5ee
James Flannery <jamioflan@hotmail.co.uk>
2012-12-29 14:29:44 +0000

    Fixed gun box recipes

--- src/minecraft/co/uk/flansmods/common/GunBoxType.java
+++ src/minecraft/co/uk/flansmods/common/GunBoxType.java
@@ -5,7 +5,10 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+import cpw.mods.fml.common.registry.GameRegistry;
+
 import net.minecraft.block.material.Material;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 
 public class GunBoxType extends InfoType
@@ -174,5 +177,110 @@ public class GunBoxType extends InfoType
 			return stack;
 		stack.stackSize = amount;
 		return stack;
+	}
+	
+	/** Reimported from old code */
+	public void addRecipe(Item par1Item)
+	{
+		if (smeltableFrom != null)
+		{
+			GameRegistry.addSmelting(getRecipeElement(smeltableFrom, 0).itemID, new ItemStack(item), 0.0F);
+		}
+		if (recipeLine == null)
+			return;
+		try
+		{
+			if (!shapeless)
+			{
+				// Fix oversized recipes
+				int rows = 3;
+				// First column
+				if (((String) recipe[0]).charAt(0) == ' ' && ((String) recipe[1]).charAt(0) == ' ' && ((String) recipe[2]).charAt(0) == ' ')
+				{
+					for (int i = 0; i < 3; i++)
+						recipe[i] = ((String) recipe[i]).substring(1);
+					// New first column
+					if (((String) recipe[0]).charAt(0) == ' ' && ((String) recipe[1]).charAt(0) == ' ' && ((String) recipe[2]).charAt(0) == ' ')
+					{
+						for (int i = 0; i < 3; i++)
+							recipe[i] = ((String) recipe[i]).substring(1);
+					}
+				}
+				// Last column
+				int last = ((String) recipe[0]).length() - 1;
+				if (((String) recipe[0]).charAt(last) == ' ' && ((String) recipe[1]).charAt(last) == ' ' && ((String) recipe[2]).charAt(last) == ' ')
+				{
+					for (int i = 0; i < 3; i++)
+						recipe[i] = ((String) recipe[i]).substring(0, last);
+					// New last column
+					last--;
+					if (((String) recipe[0]).charAt(last) == ' ' && ((String) recipe[1]).charAt(last) == ' ' && ((String) recipe[2]).charAt(last) == ' ')
+					{
+						for (int i = 0; i < 3; i++)
+							recipe[i] = ((String) recipe[i]).substring(0, 0);
+					}
+				}
+				// Top row
+				if (recipe[0].equals(" ") || recipe[0].equals("  ") || recipe[0].equals("   "))
+				{
+					Object[] newRecipe = new Object[recipe.length - 1];
+					newRecipe[0] = recipe[1];
+					newRecipe[1] = recipe[2];
+					recipe = newRecipe;
+					rows--;
+					// Next top row
+					if (recipe[0].equals(" ") || recipe[0].equals("  ") || recipe[0].equals("   "))
+					{
+						Object[] newRecipe1 = new Object[recipe.length - 1];
+						newRecipe1[0] = recipe[1];
+						recipe = newRecipe1;
+						rows--;
+					}
+				}
+				// Bottom row
+				if (recipe[rows - 1].equals(" ") || recipe[rows - 1].equals("  ") || recipe[rows - 1].equals("   "))
+				{
+					Object[] newRecipe = new Object[recipe.length - 1];
+					newRecipe[0] = recipe[0];
+					newRecipe[1] = recipe[1];
+					recipe = newRecipe;
+					rows--;
+					// Next bottom row
+					if (recipe[rows - 1].equals(" ") || recipe[rows - 1].equals("  ") || recipe[rows - 1].equals("   "))
+					{
+						Object[] newRecipe1 = new Object[recipe.length - 1];
+						newRecipe1[0] = recipe[0];
+						recipe = newRecipe1;
+						rows--;
+					}
+				}
+				for (int i = 0; i < (recipeLine.length - 1) / 2; i++)
+				{
+					recipe[i * 2 + rows] = recipeLine[i * 2 + 1].charAt(0);
+					// Split ID with . and if it contains a second part, use it
+					// as damage value.
+					if (recipeLine[i * 2 + 2].contains("."))
+						recipe[i * 2 + rows + 1] = getRecipeElement(recipeLine[i * 2 + 2].split("\\.")[0], Integer.valueOf(recipeLine[i * 2 + 2].split("\\.")[1]));
+					else
+						recipe[i * 2 + rows + 1] = getRecipeElement(recipeLine[i * 2 + 2], 0);
+				}
+				GameRegistry.addRecipe(new ItemStack(FlansMod.gunBoxBlock, recipeOutput, shortNameList.indexOf(shortName)), recipe);
+			} else
+			{
+				recipe = new Object[recipeLine.length - 1];
+				for (int i = 0; i < (recipeLine.length - 1); i++)
+				{
+					if (recipeLine[i + 1].contains("."))
+						recipe[i] = getRecipeElement(recipeLine[i + 1].split("\\.")[0], Integer.valueOf(recipeLine[i + 1].split("\\.")[1]));
+					else
+						recipe[i] = getRecipeElement(recipeLine[i + 1], 0);
+				}
+				GameRegistry.addShapelessRecipe(new ItemStack(FlansMod.gunBoxBlock, recipeOutput, shortNameList.indexOf(shortName)), recipe);
+			}
+		} catch (Exception e)
+		{
+			FlansMod.log("Failed to add recipe for : " + shortName);
+			e.printStackTrace();
+		}
 	}
 }
