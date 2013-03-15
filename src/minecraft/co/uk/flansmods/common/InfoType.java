b02b5cf45582a1562fe48f01b9f6fd4e45ba238a
James Flannery <jamioflan@hotmail.co.uk>
2013-03-15 16:36:50 +0000

    Fixed recipe adding

--- src/minecraft/co/uk/flansmods/common/InfoType.java
+++ src/minecraft/co/uk/flansmods/common/InfoType.java
@@ -239,7 +239,7 @@ public class InfoType
 		}
 		for (Item item : Item.itemsList)
 		{
-			if (item != null && item.getItemName() != null && (item.getItemName().equals("item." + arg0) || item.getItemName().equals("tile." + arg0)))
+			if (item != null && item.getUnlocalizedName() != null && (item.getUnlocalizedName().equals("item." + arg0) || item.getUnlocalizedName().equals("tile." + arg0)))
 			{
 				return new ItemStack(item, 1, damage);
 			}
