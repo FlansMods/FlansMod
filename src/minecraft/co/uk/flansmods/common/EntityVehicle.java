5df5716d49f37d5c0949644c573c2c803b297284
James Flannery <jamioflan@hotmail.co.uk>
2013-07-01 10:58:28 +0100

    Removed mouse control toggle from vehicles

--- src/minecraft/co/uk/flansmods/common/EntityVehicle.java
+++ src/minecraft/co/uk/flansmods/common/EntityVehicle.java
@@ -277,7 +277,7 @@ public class EntityVehicle extends EntityDriveable implements IEntityAdditionalS
 			}
 			case 10 : //Change Controls
 			{
-				FlansMod.proxy.changeControlMode((EntityPlayer) this.riddenByEntity);
+				//FlansMod.proxy.changeControlMode((EntityPlayer) this.riddenByEntity);
 				return true;
 			}
 		}
