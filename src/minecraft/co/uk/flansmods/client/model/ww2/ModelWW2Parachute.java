bf9ed7e5a75c7f3b9e7569e4679973ed0e8201ef
James Flannery <jamioflan@hotmail.co.uk>
2013-09-01 17:04:02 +0100

    WW2 Parachute added

--- src/minecraft/co/uk/flansmods/client/model/ww2/ModelWW2Parachute.java
+++ src/minecraft/co/uk/flansmods/client/model/ww2/ModelWW2Parachute.java
@@ -11,27 +11,27 @@ public class ModelWW2Parachute extends ModelBase
 
 	public ModelWW2Parachute()
 	{
-		backpackModel = new ModelRendererTurbo(this, 0, 0, 128, 64);
+		backpackModel = new ModelRendererTurbo(this, 0, 0, 128, 128);
 		backpackModel.addBox(-4F, 26F, -6F, 8, 12, 2);
 		
 		//Outside
-		parachuteModel[0] = new ModelRendererTurbo(this, 0, 16, 128, 64);
+		parachuteModel[0] = new ModelRendererTurbo(this, 0, 16, 128, 128);
 		parachuteModel[0].addTrapezoid(-12, 38, -12, 24, 24, 24, 0F, -8F, ModelRendererTurbo.MR_TOP);
-		parachuteModel[1] = new ModelRendererTurbo(this, 72, 0, 128, 64);
+		parachuteModel[1] = new ModelRendererTurbo(this, 72, 0, 128, 128);
 		parachuteModel[1].addTrapezoid(-8, 62, -8, 16, 8, 16, 0F, 4F, ModelRendererTurbo.MR_TOP);
-		parachuteModel[2] = new ModelRendererTurbo(this, 24, 0, 128, 64);
-		parachuteModel[2].addTrapezoid(-4, 70, -4, 8, 4, 8, 0F, 4F, ModelRendererTurbo.MR_TOP);
+		parachuteModel[2] = new ModelRendererTurbo(this, 0, 64, 128, 128);
+		parachuteModel[2].addTrapezoid(-8, 70, -8, 16, 4, 16, 0F, -4F, ModelRendererTurbo.MR_BOTTOM);
 		
 		//Inside
-		parachuteModel[3] = new ModelRendererTurbo(this, 0, 16, 128, 64);
+		parachuteModel[3] = new ModelRendererTurbo(this, 0, 16, 128, 128);
 		parachuteModel[3].flip = true;
 		parachuteModel[3].addTrapezoid(-12, 38, -12, 24, 24, 24, 0F, -8F, ModelRendererTurbo.MR_TOP);
-		parachuteModel[4] = new ModelRendererTurbo(this, 72, 0, 128, 64);
+		parachuteModel[4] = new ModelRendererTurbo(this, 72, 0, 128, 128);
 		parachuteModel[4].flip = true;
 		parachuteModel[4].addTrapezoid(-8, 62, -8, 16, 8, 16, 0F, 4F, ModelRendererTurbo.MR_TOP);
-		parachuteModel[5] = new ModelRendererTurbo(this, 24, 0, 128, 64);
+		parachuteModel[5] = new ModelRendererTurbo(this, 0, 64, 128, 128);
 		parachuteModel[5].flip = true;
-		parachuteModel[5].addTrapezoid(-4, 70, -4, 8, 4, 8, 0F, 4F, ModelRendererTurbo.MR_TOP);
+		parachuteModel[5].addTrapezoid(-8, 70, -8, 16, 4, 16, 0F, -4F, ModelRendererTurbo.MR_BOTTOM);
 	}
 
 	@Override
