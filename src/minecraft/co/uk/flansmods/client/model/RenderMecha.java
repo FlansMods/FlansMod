5d3c43e77f0bb6d267311ccfd0c5432e2169c863
James Flannery <jamioflan@hotmail.co.uk>
2013-10-11 11:28:37 +0100

    Saws and whatnot

--- src/minecraft/co/uk/flansmods/client/model/RenderMecha.java
+++ src/minecraft/co/uk/flansmods/client/model/RenderMecha.java
@@ -262,6 +262,7 @@ public class RenderMecha extends Render
 				}
 				toolType.model.renderDrill(mecha, dT);
 				GL11.glPopMatrix();
+				toolType.model.renderSaw(mecha, dT, (leftHand && mecha.leftMouseHeld) || (!leftHand && mecha.rightMouseHeld));
 			}
 		}
 		else
