349419b7234293540a9957ca16af73512d604472
godgodgodgo <god@mineycdev.com>
2014-01-08 19:51:51 +0000

    Move everything for 1.7 gradle
    + some cleanup in the assets and remove changelog since it's not used.

---
+++ src/main/java/co/uk/flansmods/client/tmt/Shape3D.java
@@ -0,0 +1,12 @@
+package co.uk.flansmods.client.tmt;
+
+public class Shape3D
+{
+	public Shape3D(PositionTransformVertex[] verts, TexturedPolygon[] poly)
+	{
+		vertices = verts;
+		faces = poly;
+	}
+	public PositionTransformVertex[] vertices;
+	public TexturedPolygon[] faces;
+}
