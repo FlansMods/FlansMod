94d62d74fdce6ea17877f663fc8ac513e57f2042
Thomas Williams <t.r.williamst@googlemail.com>
2014-01-08 00:41:17 +0000

    Added methods for creating EntityBullets
    Kind of duplicate methods for creating bullet entities, but can be
    overriden to allow sub classes of EntityBullet to be used

--- src/minecraft/co/uk/flansmods/common/guns/ItemBullet.java
+++ src/minecraft/co/uk/flansmods/common/guns/ItemBullet.java
@@ -2,11 +2,17 @@ package co.uk.flansmods.common.guns;
 
 import java.util.List;
 
-import co.uk.flansmods.common.FlansMod;
 import net.minecraft.client.renderer.texture.IconRegister;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.Vec3;
+import net.minecraft.world.World;
+import co.uk.flansmods.common.FlansMod;
+import co.uk.flansmods.common.InfoType;
+import co.uk.flansmods.common.vector.Vector3f;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
@@ -49,4 +55,35 @@ public class ItemBullet extends Item
 	}
     
 	public BulletType type;
+
+	//Can be overriden to allow new types of bullets to be created, for planes
+	public EntityBullet getEntity(World worldObj, Vec3 origin, float yaw,
+			float pitch, double motionX, double motionY, double motionZ,
+			EntityLivingBase shooter,float gunDamage, int itemDamage, InfoType shotFrom) 
+	{
+		return new EntityBullet(worldObj, origin, yaw, pitch, motionX, motionY, motionZ, shooter, gunDamage, this.type, shotFrom);
+	}
+
+	//Can be overriden to allow new types of bullets to be created, vector constructor
+	public EntityBullet getEntity(World worldObj, Vector3f origin, Vector3f direction,
+			EntityLivingBase shooter, float spread, float damage, float speed, int itemDamage, InfoType shotFrom)
+	{
+		return new EntityBullet(worldObj, origin, direction, shooter, spread, damage, this.type, speed, shotFrom);
+	}
+
+	//Can be overriden to allow new types of bullets to be created, AA/MG constructor
+	public Entity getEntity(World worldObj, Vec3 origin, float yaw,
+			float pitch, EntityLivingBase shooter, float spread, float damage,
+			int itemDamage, InfoType shotFrom) 
+	{
+		return new EntityBullet(worldObj, origin, yaw, pitch, shooter, spread, damage, this.type, shotFrom);
+	}
+
+	//Can be overriden to allow new types of bullets to be created, Handheld constructor
+	public Entity getEntity(World worldObj, EntityLivingBase player,
+			float bulletSpread, float damage, float bulletSpeed, boolean b,
+			int itemDamage, InfoType shotFrom) 
+	{
+		return new EntityBullet(worldObj, player, bulletSpread, damage, this.type, bulletSpeed, b, shotFrom);
+	}
 }
