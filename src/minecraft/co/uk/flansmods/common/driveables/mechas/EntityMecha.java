5f5b63f4ad743cee2034cc6f59483c9b5331229d
Thomas Williams <t.r.williamst@googlemail.com>
2014-01-26 01:13:09 +0000

    Refixed #349
    EntityLivingBase vs EntityLiving grr

--- src/minecraft/co/uk/flansmods/common/driveables/mechas/EntityMecha.java
+++ src/minecraft/co/uk/flansmods/common/driveables/mechas/EntityMecha.java
@@ -494,7 +494,7 @@ public class EntityMecha extends EntityDriveable
 				
 		if(!worldObj.isRemote)
 			for (int k = 0; k < gunType.numBullets; k++)
-				worldObj.spawnEntityInWorld(((ItemBullet)bulletStack.getItem()).getEntity(worldObj, bulletOrigin, armVector, (EntityLiving)(seats[0].riddenByEntity), gunType.getSpread(stack) / 2F, gunType.getDamage(stack), gunType.getBulletSpeed(stack),bulletStack.getItemDamage(), mechaType));
+				worldObj.spawnEntityInWorld(((ItemBullet)bulletStack.getItem()).getEntity(worldObj, bulletOrigin, armVector, (EntityLivingBase)(seats[0].riddenByEntity), gunType.getSpread(stack) / 2F, gunType.getDamage(stack), gunType.getBulletSpeed(stack),bulletStack.getItemDamage(), mechaType));
 		
 		if(left)
 			shootDelayLeft = gunType.mode == EnumFireMode.SEMIAUTO ? Math.max(gunType.shootDelay, 5) : gunType.shootDelay;
