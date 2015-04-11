1a4ee61b8e5f951164bf0009ba7b32bd03511373
PrototypeTheta <prototypetheta@yahoo.co.uk>
2015-04-11 18:24:44 +0100

    Front and rear leg parts

--- src/main/java/com/flansmod/client/model/ModelMecha.java
+++ src/main/java/com/flansmod/client/model/ModelMecha.java
@@ -21,6 +21,14 @@ public class ModelMecha extends ModelDriveable
 	public ModelRendererTurbo[] rightLegModel = new ModelRendererTurbo[0];
 	public ModelRendererTurbo[] leftFootModel = new ModelRendererTurbo[0];
 	public ModelRendererTurbo[] rightFootModel = new ModelRendererTurbo[0];	
+	public ModelRendererTurbo[] leftRearLegModel = new ModelRendererTurbo[0];
+	public ModelRendererTurbo[] rightRearLegModel = new ModelRendererTurbo[0];
+	public ModelRendererTurbo[] leftRearFootModel = new ModelRendererTurbo[0];
+	public ModelRendererTurbo[] rightRearFootModel = new ModelRendererTurbo[0];	
+	public ModelRendererTurbo[] leftFrontLegModel = new ModelRendererTurbo[0];
+	public ModelRendererTurbo[] rightFrontLegModel = new ModelRendererTurbo[0];
+	public ModelRendererTurbo[] leftFrontFootModel = new ModelRendererTurbo[0];
+	public ModelRendererTurbo[] rightFrontFootModel = new ModelRendererTurbo[0];	
 	public ModelRendererTurbo[] headModel = new ModelRendererTurbo[0];
 	public ModelRendererTurbo[] barrelModel = new ModelRendererTurbo[0];
 	
@@ -44,6 +52,14 @@ public class ModelMecha extends ModelDriveable
 		renderPart(rightLegModel);
 		renderPart(leftFootModel);
 		renderPart(rightFootModel);
+		renderPart(leftRearLegModel);
+		renderPart(rightRearLegModel);
+		renderPart(leftRearFootModel);
+		renderPart(rightRearFootModel);
+		renderPart(leftFrontLegModel);
+		renderPart(rightFrontLegModel);
+		renderPart(leftFrontFootModel);
+		renderPart(rightFrontFootModel);		
 		renderPart(barrelModel);
 		renderPart(headModel);
 		GL11.glPushMatrix();
@@ -127,6 +143,54 @@ public class ModelMecha extends ModelDriveable
         		model.render(f5);
 	}
 	
+	public void renderRightRearFoot(float f5, EntityMecha mecha, float f)
+	{
+        	for(ModelRendererTurbo model : rightRearFootModel)
+        		model.render(f5);
+	}
+	
+	public void renderLeftRearFoot(float f5, EntityMecha mecha, float f)
+	{
+        	for(ModelRendererTurbo model : leftRearFootModel)
+        		model.render(f5);
+	}
+	
+	public void renderRightRearLeg(float f5, EntityMecha mecha, float f)
+	{
+        	for(ModelRendererTurbo model : rightRearLegModel)
+        		model.render(f5);
+	}
+	
+	public void renderLeftRearLeg(float f5, EntityMecha mecha, float f)
+	{
+        	for(ModelRendererTurbo model : leftRearLegModel)
+        		model.render(f5);
+	}
+	
+	public void renderRightFrontFoot(float f5, EntityMecha mecha, float f)
+	{
+        	for(ModelRendererTurbo model : rightFrontFootModel)
+        		model.render(f5);
+	}
+	
+	public void renderLeftFrontFoot(float f5, EntityMecha mecha, float f)
+	{
+        	for(ModelRendererTurbo model : leftFrontFootModel)
+        		model.render(f5);
+	}
+	
+	public void renderRightFrontLeg(float f5, EntityMecha mecha, float f)
+	{
+        	for(ModelRendererTurbo model : rightFrontLegModel)
+        		model.render(f5);
+	}
+	
+	public void renderLeftFrontLeg(float f5, EntityMecha mecha, float f)
+	{
+        	for(ModelRendererTurbo model : leftFrontLegModel)
+        		model.render(f5);
+	}
+	
 	public void renderHips(float f5, EntityMecha mecha, float f)
 	{        
         	for(ModelRendererTurbo model : hipsModel)
@@ -146,6 +210,14 @@ public class ModelMecha extends ModelDriveable
 		flip(rightLegModel);
 		flip(leftFootModel);
 		flip(rightFootModel);
+		flip(leftRearLegModel);
+		flip(rightRearLegModel);
+		flip(leftRearFootModel);
+		flip(rightRearFootModel);
+		flip(leftFrontLegModel);
+		flip(rightFrontLegModel);
+		flip(leftFrontFootModel);
+		flip(rightFrontFootModel);		
 		flip(headModel);
 		flip(barrelModel);
 	}	
@@ -163,6 +235,14 @@ public class ModelMecha extends ModelDriveable
 		translate(rightLegModel, x, y, z);
 		translate(leftFootModel, x, y, z);
 		translate(rightFootModel, x, y, z);
+		translate(leftRearLegModel, x, y, z);
+		translate(rightRearLegModel, x, y, z);
+		translate(leftRearFootModel, x, y, z);
+		translate(rightRearFootModel, x, y, z);
+		translate(leftFrontLegModel, x, y, z);
+		translate(rightFrontLegModel, x, y, z);
+		translate(leftFrontFootModel, x, y, z);
+		translate(rightFrontFootModel, x, y, z);		
 		translate(headModel, x, y, z);
 		translate(barrelModel, x, y, z);
 	}
