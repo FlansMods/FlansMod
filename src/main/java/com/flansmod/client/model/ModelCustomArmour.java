24a36ed81485553cd8231ba5cf0f2b2f41558172
Chris Lane <chris@chrislane.com>
2019-01-14 19:37:26 +0000

    Fix #904

--- src/main/java/com/flansmod/client/model/ModelCustomArmour.java
+++ src/main/java/com/flansmod/client/model/ModelCustomArmour.java
@@ -7,7 +7,6 @@ import net.minecraft.client.model.ModelRenderer;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.EnumAction;
 import net.minecraft.item.ItemStack;
@@ -27,16 +26,16 @@ public class ModelCustomArmour extends ModelBiped
 	public ModelRendererTurbo[] rightLegModel = new ModelRendererTurbo[0];
 	public ModelRendererTurbo[] skirtFrontModel = new ModelRendererTurbo[0]; //Acts like a leg piece, but its pitch is set to the maximum of the two legs
 	public ModelRendererTurbo[] skirtRearModel = new ModelRendererTurbo[0]; //Acts like a leg piece, but its pitch is set to the minimum of the two legs
-
+	
 	public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5)
 	{
 		GL11.glPushMatrix();
 		GL11.glScalef(type.modelScale, type.modelScale, type.modelScale);
 		isSneak = entity.isSneaking();
 		ItemStack itemstack = ((EntityLivingBase)entity).getItemStackFromSlot(EntityEquipmentSlot.MAINHAND);
-		rightArmPose = itemstack == null || itemstack.isEmpty() ? ArmPose.EMPTY : ArmPose.ITEM;
-
-		if(itemstack != null && !itemstack.isEmpty() && entity instanceof EntityPlayer && ((EntityPlayer)entity).getItemInUseCount() > 0)
+		rightArmPose = itemstack.isEmpty() ? ArmPose.EMPTY : ArmPose.ITEM;
+		
+		if(!itemstack.isEmpty())
 		{
 			EnumAction enumaction = itemstack.getItemUseAction();
 			if(enumaction == EnumAction.BLOCK)
