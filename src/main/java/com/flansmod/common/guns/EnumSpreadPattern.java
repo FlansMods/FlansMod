a3d1e56582cd9d1773a47b3616843b91f3113956
jamioflan <jamioflan@hotmail.co.uk>
2021-01-18 14:28:27 +0000

    Fix bad spread pattern parser

--- src/main/java/com/flansmod/common/guns/EnumSpreadPattern.java
+++ src/main/java/com/flansmod/common/guns/EnumSpreadPattern.java
@@ -13,11 +13,14 @@ public enum EnumSpreadPattern
 		s = s.toLowerCase();
 		if(s.equals("circle"))
 			return circle;
-		else if(s.equals("rectangle"))
+		else if(s.equals("cube"))
 			return cube;
 		else if(s.equals("triangle"))
 			return triangle;
-		
+		else if(s.equals("horizontal"))
+			return horizontal;
+		else if(s.equals("vertical"))
+			return vertical;
 		
 		return null;
 	}
